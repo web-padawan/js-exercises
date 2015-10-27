a42a8fafed50972420ef0ac366920568a5e2b419
web-padawan <skamandr@yandex.ru>
2015-10-27 12:04:52 +0200

    [new] arrays: tests 1 - 4 complete

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,54 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
+    var len = arr.length;
 
+    for (var i = 0; i < len; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
+
+    return -1;
   },
 
   sum : function(arr) {
+    var len = arr.length,
+        sum = 0;
 
+    for (var i = 0; i < len; i++) {
+      if (typeof arr[i] === 'number') {
+        sum += arr[i];
+      }
+    }
+
+    return sum;
   },
 
   remove : function(arr, item) {
+    var len = arr.length,
+        result = [];
 
+    for (var i = 0; i < len; i++) {
+      if (arr[i] !== item) {
+        result.push(arr[i]);
+      }
+    }
+
+    return result;
   },
 
   removeWithoutCopy : function(arr, item) {
+    var len = arr.length;
 
+    for (var i = 0; i < len; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i -= 1;
+      }
+    }
+
+    return arr;
   },
 
   append : function(arr, item) {
