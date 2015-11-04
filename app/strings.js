117e19d51688ad17815b11cd616c3581a19cf0ee
web-padawan <skamandr@yandex.ru>
2015-11-04 13:04:14 +0200

    [new] strings: test 1 completed

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
+    var len = str.length,
+        arr = [str[0]];
 
+    for (var i = 1; i < len; i++) {
+      if (str[i] === str[i - 1]) {
+        arr[arr.length - 1] = arr[arr.length - 1] + str[i];
+      } else {
+        arr.push(str[i]);
+      }
+    }
+
+    var out = arr.map(function(elem) {
+      return elem.substr(0, amount);
+    });
+
+    return out.join('');
   },
   wordWrap: function(str, cols) {
 
