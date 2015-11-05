82bd6303fa561930c8ace29377d40a12c79f95d6
web-padawan <skamandr@yandex.ru>
2015-11-05 15:41:09 +0200

    [new] count: tests completed

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var c = start;
+    console.log(c);
 
+    var myTimer = setInterval(function() {
+      c++;
+      console.log(c);
+      if (c == end) {
+        clearInterval(myTimer);
+      }
+    }, 100);
+
+    return {
+      cancel : function() {
+        clearInterval(myTimer);
+      }
+    }
   }
 };
