d7d16a2e6e08d5f59eae02780f54a207929ecc60
web-padawan <skamandr@yandex.ru>
2015-11-09 08:30:59 +0200

    [new] module: test completed

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,16 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
+    var greeting = str1,
+        name = str2,
+        sayIt = function() {
+          return this.greeting + ', ' + this.name;
+        };
 
+    return {
+      name: name,
+      greeting: greeting,
+      sayIt : sayIt
+    };
   }
 };
