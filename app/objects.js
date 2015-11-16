db854ff465b658134c8921a6abd3ddf057b331fb
web-padawan <skamandr@yandex.ru>
2015-11-16 19:29:37 +0200

    [upd] objects: tests completed

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.apply(obj, arguments);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var result = [];
 
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        result.push(prop + ': ' + obj[prop]);
+      }
+    }
+
+    return result;
   }
 };
