ea661d13a94476a79f69b715c6123b293951924d
web-padawan <skamandr@yandex.ru>
2015-11-17 10:26:51 +0200

    [upd] recursion: list files completed

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var result = result || [];
 
+    for (var i = 0; i < data.files.length; i++) {
+      if (typeof data.files[i] === 'string' && (data.dir === dirName || !dirName)) {
+        result.push(data.files[i]);
+      }
+      else if (typeof data.files[i] === 'object') {
+        result = result.concat(this.listFiles(data.files[i], (data.dir === dirName || !dirName) ? '' : dirName));
+      }
+    }
+
+    return result;
   },
 
   permute: function(arr) {
